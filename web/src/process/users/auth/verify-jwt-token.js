4e463b283a14a24e607c121618fbff42f5b8839b
u40_0m_t34 <atom.tea.design@gmail.com>
2020-03-05 14:30:27 -0700

    lifecycle method renaming

--- web/src/process/users/auth/verify-jwt-token.js
+++ web/src/process/users/auth/verify-jwt-token.js
@@ -1,49 +1,49 @@
-import React from 'react'
-import jwt from 'jsonwebtoken'
+import React from "react";
+import jwt from "jsonwebtoken";
 
-import * as actions from './actions'
-import connected from '../../../state/connect'
-import { selector as user } from '../reducer'
-import authToken from '../../../utilities/local-storage/token'
+import * as actions from "./actions";
+import connected from "../../../state/connect";
+import { selector as user } from "../reducer";
+import authToken from "../../../utilities/local-storage/token";
 
 const JwtProcess = WrappedComponent => {
   class JWTVerify extends React.Component {
     constructor(props) {
-      super(props)
+      super(props);
       this.state = {
         fetching: false
-      }
+      };
     }
-    componentWillMount() {
-      const token = authToken.get()
-      const { active } = this.props.user
+    UNSAFE_componentWillMount() {
+      const token = authToken.get();
+      const { active } = this.props.user;
 
       if (token && !active) {
-        const decoded = jwt.decode(token)
-        const currentTime = (new Date().getTime()) / 1000
+        const decoded = jwt.decode(token);
+        const currentTime = new Date().getTime() / 1000;
         if (decoded.exp < currentTime) {
-          authToken.remove()
+          authToken.remove();
         } else {
           this.setState({ fetching: true }, () => {
-            this.props.userAuthActions.fetchUserByJWT()
-          })
+            this.props.userAuthActions.fetchUserByJWT();
+          });
         }
       }
     }
 
-    componentWillReceiveProps(nextProps) {
+    UNSAFE_componentWillReceiveProps(nextProps) {
       if (nextProps.user && nextProps.user.active) {
-        this.setState({ fetching: false })
+        this.setState({ fetching: false });
       }
     }
 
     render() {
-      if (this.state.fetching) return null
-      return <WrappedComponent {...this.props} />
+      if (this.state.fetching) return null;
+      return <WrappedComponent {...this.props} />;
     }
   }
 
-  return connected([user], [actions])(JWTVerify)
-}
+  return connected([user], [actions])(JWTVerify);
+};
 
-export default JwtProcess
+export default JwtProcess;
